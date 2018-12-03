85509771d4924ee165e6beaf1b4dd1e4c5cbd9b4
Mateusz Szczygielski <Mateusz.Szczygielski@netent.com>
2018-12-03 13:57:22 +0100

    Add bodyContent arg to header decorator

--- app/steps/decorateProxyReqOpts.js
+++ app/steps/decorateProxyReqOpts.js
@@ -8,7 +8,7 @@ function decorateProxyReqOpt(container) {
   var resolverFn = container.options.proxyReqOptDecorator || defaultDecorator;
 
   return Promise
-    .resolve(resolverFn(container.proxy.reqBuilder, container.user.req))
+    .resolve(resolverFn(container.proxy.reqBuilder, container.proxy.bodyContent, container.user.req))
     .then(function(processedReqOpts) {
         delete processedReqOpts.params;
         container.proxy.reqBuilder = processedReqOpts;
